cab7e37c8f72741b8295da7346dbad77e2433659
David Plesel <plesel.david@gmail.com>
2024-07-31 06:19:34 +0200

    Filling with content (V0)

--- js/design.js
+++ js/design.js
@@ -2,13 +2,23 @@ const schemes = {
     'scheme-1': {
         title: 'Balancing a clicker game : Prototype',
         description: `
-            <p class="mb-4">This project stands out as a prototype for a Web3 clicker game. Using Machinations, I am able to simulate various game features, such as manual and autoclicking:</p>
+            <p class="mb-4">
+            This project stands out as a prototype for a Web3 clicker game. Using <span class="font-bold">Machinations</span>, I am able to simulate various game features such as manual and autoclicking:
+            </p>
+            <p class="mb-4">
+                <span class="font-bold">To start, click on the button play </span>
+                <img src="https://cdn.imgbin.com/1/0/23/imgbin-line-triangle-brand-other-sony-vegas-icon-55xdYWkE4x1Y3ZdgmmQy4MEFC.jpg" alt="Play Button Logo" class="inline-block w-4 h-4">
+                <span class="font-bold"></span>
+            </p>
             <ul class="list-disc pl-5 mb-4">
-            <p class= mb-4> Click on the "play" button to start ! 
-                <li>Clicking on the <span class="text-green-500">green triangle</span> creates a resource</li>
-                <li>Clicking on the <span class="text-yellow-500">orange triangle</span> lets you create a resource generator</li>
+                <li>Clicking on the <span class="text-green-500 font-bold">green triangle</span> creates a resource</li>
+                <li>Clicking on the <span class="text-yellow-500 font-bold">orange triangle</span> creates a resource generator</li>
             </ul>
-            <p class="mb-4">Feel free to look into it directly <a href="https://my.machinations.io/d/clicker-v4/910429c003f011ee915c02f943517e50" class="text-blue-500 underline hover:text-blue-700">here</a> so you can get see the rest of the game !</p>
+            <p class="mb-4">
+                Feel free to look into it directly <a href="https://my.machinations.io/d/clicker-v4/910429c003f011ee915c02f943517e50" class="text-blue-500 font-bold underline hover:text-blue-700">here</a> 
+                so you can get see the rest of the game!
+            </p>
+
         `,
         featuredImage: 'https://my.machinations.io/d/clicker-v4/910429c003f011ee915c02f943517e50',
     },
@@ -17,11 +27,12 @@ const schemes = {
         description:  `
             <p class="mb-4">This project stands out as a prototype for a Web3 clicker game. Using Machinations, I am able to simulate various game features, such as manual and autoclicking:</p>
             <ul class="list-disc pl-5 mb-4">
-            <p class= mb-4> Click on the "play" button to start ! 
+            <p class= mb-4> Click on the "play" button to start !
                 <li>Clicking on the <span class="text-green-500">green triangle</span> creates a resource</li>
                 <li>Clicking on the <span class="text-yellow-500">orange triangle</span> lets you create a resource generator</li>
             </ul>
-            <p class="mb-4">Feel free to look into it directly <a href="https://my.machinations.io/d/clicker-v4/910429c003f011ee915c02f943517e50" class="text-blue-500 underline hover:text-blue-700">here</a> so you can get see the rest of the game !</p>
+            <p class="mb-4">Feel free to look into it directly <a href="https://my.machinations.io/d/clicker-v4/910429c003f011ee915c02f943517e50" class="text-blue-500 underline hover:text-blue-700">here</a> 
+            so you can get see the rest of the game !</p>
     `,
         featuredImage: 'https://docs.google.com/presentation/d/e/2PACX-1vRaSd5QmInQi7p8LRfcSb3qMmeIDMx0VNy0OFoP1GG849csJ6RPo82TXjOQ_tlPpRQgyblQCpQEET5I/embed?start=false&loop=true&delayms=500',
     },
--- js/games.js
+++ js/games.js
@@ -1,19 +1,26 @@
 const projects = {
     'project-1': {
-        title: 'Heavy Metal - The Hunt',
-        description: 'This is a brief description of Project One. It showcases the main features and technologies used.',
+        title: 'Heavy Metal - The Hunt <span class="text-gray-500 italic font-bold">(2023)</span><br><span class="inline-block bg-blue-500 text-white text-sm px-2 py-1 rounded-full mt-2">Gameplay Designer</span>',
+        description: `
+            <p class="mb-4 mt-2">HV-MTL - The Hunt is a roguelite game made by <span class="font-bold">Yuga labs</span> and developed by <span class="font-bold">MakeMePulse</span> </p>
+            <p class="mb-4 mt-2">I created and documented the game mechanics for the technical and design teams and contributed to implementing the game logic in collaboration with the client.</p>
+            <span class="font-bold">Task overview</span>
+            <ul class="list-disc pl-5 mb-4">
+                <li>Worked with <span class="font-bold">Figma </span>, creating <span class="font-bold">UI wireframes </span>and diagram the game design </span></li>
+                <li>Updating and managing game documentation with <span class="font-bold"> Jira </span> and <span class="font-bold">Confluence </span></li>
+            </ul>
+        `,
         featuredImage: 'https://www.youtube.com/embed/M7-Y4Lm8EAI?autoplay=1&mute=1&loop=1&modestbranding=1&rel=0&controls=0&playlist=M7-Y4Lm8EAI&modestbranding=1&showinfo=0&iv_load_policy=3&rel=0',
         link: 'https://hv-mtl.com/'
-        
     },
     'project-2': {
-        title: 'The Infernity',
+        title: 'The Infernity <span class="text-gray-500 italic font-bold">(2022 - 2023)</span><br><span class="inline-block bg-red-500 text-white text-sm px-2 py-1 rounded-full mt-2">Game Economy Designer</span>  <span class="inline-block bg-blue-500 text-white text-sm px-2 py-1 rounded-full mt-2">Gameplay Designer</span>',
         description: 'This is a brief description of Project Two. It highlights the key functionalities and tools utilized.',
         featuredImage: 'https://www.youtube.com/embed/6s_a8YN04Qc?autoplay=1&mute=1&controls=0&loop=1&playlist=6s_a8YN04Qc&modestbranding=1&showinfo=0&rel=0',
         link: 'https://theinfernity.com/game'
     },
     'project-3': {
-        title: 'Time Breachers',
+        title: 'Time Breachers <span class="text-gray-500 italic font-bold">(2021 - 2022)</span><br><span class="inline-block bg-red-500 text-white text-sm px-2 py-1 rounded-full mt-2">Game Economy Designer</span>',
         description: 'This is a brief description of Project Three. It includes a summary of the project\'s scope and outcomes.',
         featuredImage: 'https://www.youtube.com/embed/jT_V_R_shHg?autoplay=1&mute=1&controls=0&loop=1&playlist=jT_V_R_shHg&modestbranding=1&showinfo=0&iv_load_policy=3&rel=0',
         link: 'https://play.google.com/store/apps/details?id=com.StarChainGazer.TimeBreachers&hl=en_US'
@@ -34,8 +41,8 @@ function getQueryParam(name) {
 function showProject(projectId) {
     const project = projects[projectId];
     if (project) {
-        document.getElementById('featured-title').innerText = project.title;
-        document.getElementById('featured-description').innerText = project.description;
+        document.getElementById('featured-title').innerHTML = project.title;
+        document.getElementById('featured-description').innerHTML = project.description;
 
         const featuredProject = document.getElementById('featured-project');
         const existingMediaContainer = featuredProject.querySelector('.featured-media');
